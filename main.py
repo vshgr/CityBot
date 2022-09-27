2c678521367dc233ce612bf1d6f8bb5f8e1f29f6
alisa <alisavy2010@gmail.com>
2022-09-27 20:02:59 +0300

    update

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import requests
 import telebot
 from telebot import types
 import pygsheets
 
+user_dict = {}
 gc = pygsheets.authorize(service_file='credentionals.json')
 
 sheets = gc.open('Места СПБ')
@@ -10,151 +10,181 @@ worksheet = sheets[0]
 
 token = "5365557057:AAEE2nGlJvMxYUA7TEmAJ36dH1lHj7eBd0M"
 bot = telebot.TeleBot('5365557057:AAEE2nGlJvMxYUA7TEmAJ36dH1lHj7eBd0M')
-chat_id = "838481251"
-
-NAME = ""
-ADRESS = ""
-METRO = ""
-SCHEDULE = ""
-DESCRIPTION = ""
-TAGS = []
-URL = ""
-MARK = ""
-
-tags = []
 
 
-def send_msg(text):
-    url = "https://api.telegram.org/bot" + token + "/sendMessage?chat_id=" + chat_id + "&text=" + text
-    results = requests.get(url)
-    return results.json()
-
-
-send_msg("Привет! Мы создаём приложение для путешественников, "
-         "и ты в этом нам можешь помочь! Расскажи о своих любимых местах в СПБ")
+class User:
+    def __init__(self, ident):
+        self.ident = ident
+        self.NAME = ""
+        self.ADRESS = ""
+        self.METRO = ""
+        self.SCHEDULE = ""
+        self.DESCRIPTION = ""
+        self.TAGS = []
+        self.URL = ""
+        self.MARK = ""
+        self.tags = []
 
 
 @bot.message_handler(commands=["start"])
 def start(message):
-    global tags
-    tags = ["Культурные пространства",
-            "Открытые пространства",
-            "Масштабные кинопоказы",
-            "Бары",
-            "Парки развлечений",
-            "Крыши/смотровые площадки",
-            "Планетарий",
-            "Пикник",
-            "Кулинарные шоу/дегустации",
-            "Уютные заведения",
-            "Ботанические сады",
-            "Романтические кинопоказы",
-            "Уличное искусство",
-            "Андеграундные мероприятия",
-            "Необычные дворы",
-            "Фитнес туризм",
-            "Тур на воде",
-            "Тур по воздуху",
-            "Поход",
-            "Архитектурный тур",
-            "Театральное искусство",
-            "Художественное искусство",
-            "Музыкальное искусство",
-            "Арт-пространства",
-            "Экологический туризм",
-            "Wellness туризм",
-            "Прогулки",
-            "Шоппинг",
-            "Камерные встречи",
-            "Завершить"]
-
-    msg = bot.send_message(message.chat.id, "Для начала введи название места", reply_markup=types.ReplyKeyboardRemove())
+    chat = message.chat.id
+    ident = message.from_user.id
+    userid = User(ident)
+    user_dict[chat] = userid
+    msg = bot.send_message(chat, "Для начала введи название места", reply_markup=types.ReplyKeyboardRemove())
     bot.register_next_step_handler(msg, get_name)
 
 
 def get_name(message):
-    global NAME
-    NAME = message.text
-    msg = bot.send_message(message.chat.id, "Введи адрес места")
+    if message.text == "/restart":
+        start(message)
+    chat = message.chat.id
+    name = message.text
+    userid = user_dict[chat]
+    userid.NAME = name
+    userid.tags = ["Культурные пространства",
+                   "Открытые пространства",
+                   "Кинопоказы",
+                   "Клубы & бары",
+                   "Парки развлечений",
+                   "Уличное искусство",
+                   "Необычные дворы",
+                   "Крыши / смотровые площадки",
+                   "Скейтпарки",
+                   "Архитектурный тур",
+                   "Театральное искусство",
+                   "Музеи и выставки",
+                   "Музыкальное искусство",
+                   "Арт - пространства",
+                   "Тематические встречи",
+                   "Прогулки",
+                   "Шоппинг",
+                   "Уютные заведения",
+                   "Рестораны & дегустации",
+                   "Зоо пространства",
+                   "Завершить"]
+    msg = bot.send_message(chat, "Введи адрес места")
     bot.register_next_step_handler(msg, get_adress)
 
 
 def get_adress(message):
-    global ADRESS
-    ADRESS = message.text
-    msg = bot.send_message(message.chat.id, "Укажи ближайшую станцию метро")
+    if message.text == "/restart":
+        start(message)
+    chat = message.chat.id
+    adress = message.text
+    userid = user_dict[chat]
+    userid.ADRESS = adress
+    msg = bot.send_message(chat, "Укажи ближайшую станцию метро")
     bot.register_next_step_handler(msg, get_metro)
 
 
 def get_metro(message):
-    global METRO
-    METRO = message.text
-    msg = bot.send_message(message.chat.id, "Введи часы работы места, если они есть. Если их нет введи '-'")
+    if message.text == "/restart":
+        start(message)
+    chat = message.chat.id
+    metro = message.text
+    userid = user_dict[chat]
+    userid.METRO = metro
+    msg = bot.send_message(chat, "Введи часы работы места, если они есть. Если их нет введи '-'")
     bot.register_next_step_handler(msg, get_schedule)
 
 
 def get_schedule(message):
-    global SCHEDULE
-    SCHEDULE = message.text
+    if message.text == "/restart":
+        start(message)
+    chat = message.chat.id
+    schedule = message.text
+    userid = user_dict[chat]
+    userid.SCHEDULE = schedule
     rmk = types.ReplyKeyboardMarkup()
-    for i in tags:
+    for i in userid.tags:
         rmk.add(types.KeyboardButton(i))
-    msg = bot.send_message(message.chat.id, "Выбери теги", reply_markup=rmk)
+    msg = bot.send_message(chat, "Выбери теги", reply_markup=rmk)
     bot.register_next_step_handler(msg, get_tags)
 
 
 def get_tags(message):
+    bot.send_message(message.chat.id, "Добавляем...", reply_markup=types.ReplyKeyboardRemove())
+    chat = message.chat.id
+    tag = message.text
+    userid = user_dict[chat]
+    if message.text == "/restart":
+        start(message)
+        userid.TAGS.clear()
     rmk = types.ReplyKeyboardMarkup()
     if message.text == "Завершить":
         msg = bot.send_message(message.chat.id, "Спасибо, теперь добавь ссылку с описанием",
                                reply_markup=types.ReplyKeyboardRemove())
         bot.register_next_step_handler(msg, get_url)
     else:
-        global TAGS
-        TAGS.append(message.text)
-        tags.remove(message.text)
-        for i in tags:
+        userid.TAGS.append(tag)
+        for i in userid.tags:
             rmk.add(types.KeyboardButton(i))
         msg = bot.send_message(message.chat.id, "Можешь выбрать больше тегов", reply_markup=rmk)
         bot.register_next_step_handler(msg, get_tags)
 
 
 def get_url(message):
-    global URL
-    URL = message.text
-    msg = bot.send_message(message.chat.id, "Если вам есть, что добавить от себя об этом месте, введите в строку ниже,"
-                                            " иначе введите '-'")
+    chat = message.chat.id
+    url = message.text
+    userid = user_dict[chat]
+    if message.text == "/restart":
+        start(message)
+        userid.TAGS.clear()
+    userid.URL = url
+    msg = bot.send_message(chat, "Если вам есть, что добавить от себя об этом месте, введите в строку ниже,"
+                                 " иначе введите '-'")
     bot.register_next_step_handler(msg, get_desc)
 
 
 def get_desc(message):
-    global DESCRIPTION
-    DESCRIPTION = message.text
+    chat = message.chat.id
+    desc = message.text
+    userid = user_dict[chat]
+    userid.DESCRIPTION = desc
+    if message.text == "/restart":
+        start(message)
+        userid.TAGS.clear()
     rmk = types.ReplyKeyboardMarkup()
     rmk.add(types.KeyboardButton("1"), types.KeyboardButton("2"),
             types.KeyboardButton("3"), types.KeyboardButton("4"),
             types.KeyboardButton("5"))
-    msg = bot.send_message(message.chat.id, "Дайте оценку", reply_markup=rmk)
+    msg = bot.send_message(chat, "Дайте оценку", reply_markup=rmk)
     bot.register_next_step_handler(msg, get_mark)
 
 
 def get_mark(message):
-    global MARK
-    MARK = message.text
-    bot.send_message(message.chat.id, "*Ответ записан!*", reply_markup=types.ReplyKeyboardRemove(),
+    chat = message.chat.id
+    mark = message.text
+    userid = user_dict[chat]
+    userid.MARK = mark
+    if message.text == "/restart":
+        start(message)
+        userid.TAGS.clear()
+    bot.send_message(message.chat.id, "*Ответ записан!*\n Можешь добавить ещё одно место",
+                     reply_markup=types.ReplyKeyboardRemove(),
                      parse_mode='markdown')
-    row = [NAME, ADRESS, METRO, SCHEDULE, get_string(TAGS), URL, DESCRIPTION, MARK]
+    row = [userid.NAME, userid.ADRESS, userid.METRO, userid.SCHEDULE, get_string(userid.TAGS), userid.URL,
+           userid.DESCRIPTION, userid.MARK]
     sheets[0].insert_rows(1, number=1, values=row, inherit=False)
-    TAGS.clear()
-    send_msg("Можешь добавить ещё одно место")
+    userid.TAGS.clear()
+    bot.send_message(chat, "Можешь добавить ещё одно место")
     start(message)
 
 
 def get_string(tags):
     s = ""
     for i in tags:
-        s = s + i + " "
+        if i == tags[-1]:
+            s = s + i
+        else:
+            s = s + i + "\n"
     return s
 
 
-bot.polling()
+while True:
+    try:
+        bot.polling()
+    except:
+        continue
